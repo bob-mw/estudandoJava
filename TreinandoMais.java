7c0146049ba4b9832107dedeb4e25dafee06b0b3
Bob <wend3llsouza@gmail.com>
2021-05-08 19:52:34 -0300

    :package: testando java com Debian

---
+++ TreinandoMais.java
@@ -0,0 +1,7 @@
+public class TreinandoMais {
+	public static void main(String[] args) {
+		System.out.println("Testando Java RunTime no Linux");
+		System.out.println("Até agora pegou perfeitamente");
+	}
+
+}
