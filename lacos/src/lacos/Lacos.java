8394e230c7e1d670bf7be9c3cbe8de9f22e4fbad
wend3ll-souza <wend3llsouza@gmail.com>
2021-05-09 00:44:53 -0300

    :package: laços encadeados, se divertindo com triangulos

--- lacos/src/lacos/Lacos.java
+++ lacos/src/lacos/Lacos.java
@@ -24,6 +24,20 @@ public class Lacos {
 		for (int count2 = 1; count2 <= 10; count2++) {
 			System.out.println("contando"+count2);
 		}
+		
+		System.out.println("   ");
+		
+		System.out.println("Laços encadeados");
+		
+		for (int linha = 0; linha <= 10; linha++) {
+			for (int coluna = 0; coluna <= 10; coluna++) {
+				if (coluna > linha) {
+					break;
+				}
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 	}
 
 }
