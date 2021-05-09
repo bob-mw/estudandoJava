eec19419abacc8434a08205c2ec26994d229d56b
Bob <wend3llsouza@gmail.com>
2021-05-08 22:50:00 -0300

    :package: sismate laços com while

--- brincandoComNumerosJava/src/brincandoComNumerosJava/Escopo.java
+++ brincandoComNumerosJava/src/brincandoComNumerosJava/Escopo.java
@@ -3,7 +3,7 @@ package brincandoComNumerosJava;
 public class Escopo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ lacos/src/lacos/Lacos.java
@@ -0,0 +1,20 @@
+package lacos;
+
+public class Lacos {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Brincando com Laços");
+		
+		int count = 0;
+		while(count <= 10) {
+			System.out.println(count);
+			count++;
+		}
+		
+		/*for (int count = 1; count >= 10; count += count) {
+			System.out.println("contando");
+		}*/
+	}
+
+}
