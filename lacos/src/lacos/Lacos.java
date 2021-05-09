960524714a597fda413baa61de7a4857daa0a7d1
wend3ll-souza <wend3llsouza@gmail.com>
2021-05-08 23:51:52 -0300

    :package: laços | incrementador

--- lacos/src/lacos/Lacos.java
+++ lacos/src/lacos/Lacos.java
@@ -4,13 +4,19 @@ public class Lacos {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Brincando com LaÃ§os");
+		System.out.println("Brincando com Laços");
 		
 		int count = 0;
-		while(count <= 10) {
+		int total = 0;
+		
+		while(count < 10) {
 			System.out.println(count);
 			count++;
+			total += count;
 		}
+		
+		System.out.println("O total é: " + total);
+		
 		
 		/*for (int count = 1; count >= 10; count += count) {
 			System.out.println("contando");
